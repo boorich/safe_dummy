602d027fc818627f6f4f5b8c89134762fb855172
empea-careercriminal <empeamtk@googlemail.com>
2023-11-28 09:55:28 +0100

    debug: CREATE2 transaction Cleaning up debugging

--- index.ts
+++ index.ts
@@ -11,7 +11,11 @@ const abi = [{"anonymous":false,"inputs":[{"indexed":false,"internalType":"contr
 const data = '0x1688f0b90000000000000000000000003e5c63644e683549055b9be8653de26e0b4cd36e0000000000000000000000000000000000000000000000000000000000000060b1073742015cbcf5a3a4d9d1ae33ecf619439710b89475f92e2abd2117e90f900000000000000000000000000000000000000000000000000000000000000164b63e800d0000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000140000000000000000000000000f48f2b2d2a534e402487b3ee7c18c33aec0fe5e40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000d1b9177cb774bde9756631cbf567a6d7e039b4fc000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'; // Replace with your input data
 const iface = new ethers.utils.Interface(abi);
 const decoded = iface.parseTransaction({ data });
-console.log(decoded);
+
+// Define an interface for safeSdk
+interface SafeSdk {
+    getAddress: () => Promise<string>;
+}
 
 // https://chainlist.org/?search=goerli&testnets=true
 const RPC_URL = 'https://eth-goerli.public.blastapi.io'
@@ -24,37 +28,49 @@ const ethAdapterOwner1 = new EthersAdapter({
     ethers,
     signerOrProvider: owner1Signer
 })
+
 // log all fields of the adapter
 async function logAdapter() {
+    //the entire tx object
+    console.log(decoded);
+    //signer address and blance
     try {
         const signerAddress = await ethAdapterOwner1.getSignerAddress();
+        if (typeof signerAddress !== 'string') {
+            throw new Error('Signer address is undefined');
+        }
         console.log(`Signer address: ${signerAddress}`);
+        const balance = await provider.getBalance(signerAddress);
+        console.log(`Signer balance: ${ethers.utils.formatEther(balance)} ETH`);
     } catch (error) {
-        console.error('Failed to get signer address:', error);
+        console.error('Failed to get signer address or balance:', error);
     }
 }
 
 // Check if address is a contract
 async function isContractDeployed(address: string): Promise<boolean> {
-    const code = await provider.getCode(address);
-    return code !== "0x";
+    try {
+        const code = await provider.getCode(address);
+        const isDeployed = code !== "0x";
+        console.log(`Contract deployed at ${address}: ${isDeployed}`);
+        return isDeployed;
+    } catch (error) {
+        console.error(`Error checking contract deployment at ${address}:`, error);
+        return false;
+    }
 }
 
-// Define an interface for safeSdk
-interface SafeSdk {
-    getAddress: () => Promise<string>;
-}
 
 async function initAPIKit() {
     const txServiceUrl = 'https://safe-transaction-goerli.safe.global'
     const safeService = new SafeApiKit({ txServiceUrl, ethAdapter: ethAdapterOwner1 })
-    return safeService
+    // return safeService
     // console.log(safeService)
 }
 
 
 async function createSafe(): Promise<SafeSdk> {
-    const address = "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2"; // Replace with the address calculated by the CREATE2 opcode
+    const address = ""; // Replace with the address calculated by the CREATE2 opcode
     const contractExists = await isContractDeployed(address);
     if (contractExists) {
         console.log(`A contract already exists at address ${address}`);
@@ -106,7 +122,9 @@ async function sendEth(safeSdk: SafeSdk) {
 initAPIKit()
 logAdapter()
 createSafe()
-// Handle promises
+// isContractDeployed('0x3e5c63644e683549055b9be8653de26e0b4cd36e')
+
+// Handle as promises
 // initAPIKit().then(() => {
 //     return createSafe();
 // }).then((safeSdk) => {
